615789eba845e421297b19b1e77a61163093ff23
benny cheng <benny12628@gmail.com>
2022-07-29 22:50:56 +0800

    Testing Non-Authed Requests

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,43 +1,10 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
-// Routes
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-// Middlewares
-import { errorHandler } from './middlewares/error-handler';
-// Errors
-import { NotFoundError } from './errors/not-found-error';
+import { app } from './app';
 
 const PORT = 3000;
 const AUTH_MONGO_SERVICE_HOST = process.env.AUTH_MONGO_SERVICE_HOST;
 const AUTH_MONGO_SERVICE_PORT = process.env.AUTH_MONGO_SERVICE_PORT;
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.get('*', async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
