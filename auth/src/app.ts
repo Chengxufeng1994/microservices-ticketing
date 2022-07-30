0bd4120f822869b35d32590fa1dbe7c15f26007e
benny cheng <benny12628@gmail.com>
2022-07-30 09:57:23 +0800

    Code Sharing

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,16 +2,13 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@msa-tickets/common';
 
 // Routes
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-// Middlewares
-import { errorHandler } from './middlewares/error-handler';
-// Errors
-import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.set('trust proxy', true);
