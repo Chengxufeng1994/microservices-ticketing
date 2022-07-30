69e44f0f8a5c59af0f394c85fc57d1b2007d45bc
benny cheng <benny12628@gmail.com>
2022-07-30 08:27:14 +0800

    Create SSR React App

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
+
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push('/'),
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="mb-3">
+        <label className="form-label">Email Address</label>
+        <input
+          className="form-control"
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        ></input>
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Password</label>
+        <input
+          type="password"
+          value={password}
+          className="form-control"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        ></input>
+      </div>
+      {errors}
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
