baa880ff2473da1c67fe8abfa01207b14f82f4f3
benny cheng <benny12628@gmail.com>
2022-08-13 14:41:20 +0800

    Expiration service update

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,4 +1,6 @@
 import Queue from 'bull';
+import { natsWrapper } from '../nats-wrapper';
+import { ExpirationCompletePublisher } from '../events/publisher/expiration-complete-publisher';
 
 interface IPayload {
   orderId: string;
@@ -9,10 +11,14 @@ const expirationQueue = new Queue<IPayload>('order:expiration', {
 });
 
 expirationQueue.process(async (job, done) => {
-  console.log(
-    'I want to publish an expiration:complete event for orderId',
-    job.data.orderId
-  );
+  // console.log(
+  //   'I want to publish an expiration:complete event for orderId',
+  //   job.data.orderId
+  // );
+
+  await new ExpirationCompletePublisher(natsWrapper.client).publish({
+    orderId: job.data.orderId,
+  });
 
   done();
 });
